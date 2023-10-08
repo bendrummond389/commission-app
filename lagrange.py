e7ffea86659f56c1fb15600c94efbf7281201080
bendrummond389 <60265150+bendrummond389@users.noreply.github.com>
2023-10-07 23:39:12 -0600

    done

---
+++ lagrange.py
@@ -0,0 +1,21 @@
+import sympy as sp
+
+points = [(0, 0), (2000, 400), (10000, 1200), (20000, 1300)]
+
+# Define the variable
+x = sp.symbols('x')
+
+# init polynomial
+P = 0
+
+# calculate lagrange polynomial
+for i, (xi, yi) in enumerate(points):
+    li = 1
+    for j, (xj, yj) in enumerate(points):
+        if i != j:
+            li *= (x - xj) / (xi - xj)
+    P += yi * li
+
+# simplify and print
+P = sp.simplify(P)
+print(P)
